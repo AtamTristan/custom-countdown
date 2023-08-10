18a50caf02bf4ee63a028163f1f4ef4f4772e4ea
tristanwesendahlvelazquez <tristan.wesendahl@hotmail.com>
2023-08-10 23:39:41 +0200

    Form Values

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input minimum with today's date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Takes Values from Form Input
+function updateCountdown(e){
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+}
+
+// Event Listener
+countdownForm.addEventListener('submit', updateCountdown);
