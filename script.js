652ea09ac3bf4455b647c1beaba235aede3b9089
tristanwesendahlvelazquez <tristan.wesendahl@hotmail.com>
2023-08-10 23:23:23 +0200

    Building UI Components

---
+++ script.js
@@ -0,0 +1,3 @@
+const inputContainer = document.getElementById('input-container');
+const countdownForm = document.getElementById('countdownForm');
+const delEl = document.getElementById('date-picker');
